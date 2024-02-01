e2c0fa62b87e7744cf2df5d55d224c063ca08cab
Andres-Siabato <andres.siabato01@gmail.com>
2024-02-01 13:47:40 -0500

    commit incial

---
+++ Tipos/02 - Strings.py
@@ -0,0 +1,13 @@
+nombre_curso = "Ultimate Python"
+
+description_curso = """
+Ultimate Python
+Las 3 comillas sirven para un texto largo
+Prueba
+"""
+
+print(description_curso)
+
+print(len(nombre_curso))
+
+print(nombre_curso[0])
